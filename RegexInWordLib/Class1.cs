38de54b622d8e1afd517b96d3108fd2efb4c7ad9
Admin <Admin@LAPTOP-4HRM131V>
2025-05-17 22:01:30 +0300

    Add project files.

---
+++ RegexInWordLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexInWordLib
+{
+    public class Class1
+    {
+    }
+}
